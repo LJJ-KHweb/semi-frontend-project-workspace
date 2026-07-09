f1b48160383eb86f1a568821485307250d66e548
kkimmun <ksk01099645771@gmail.com>
2026-07-09 10:43:18 +0900

    fix : 이거왜안됨?

--- src/features/boards/require/Require.jsx
+++ src/features/boards/require/Require.jsx
@@ -28,14 +28,71 @@ const Require = () => {
     api.get(`/requires?page=${page + 1}&size=${pages.size}`).then((result) => {
       console.log(result.data.data);
 
-      setRequires(result.data.data);
+      setRequires(result.data.data.requires);
+      setPages(result.data.data.pageInfo);
     });
   }, [page]);
   const totalPages = Math.ceil(pages.boardCounts / pages.size);
   const currentGroup = Math.floor(page / PAGE_GROUP_SIZE);
   const groupStart = currentGroup * PAGE_GROUP_SIZE;
   const groupEnd = Math.min(groupStart + PAGE_GROUP_SIZE, totalPages);
-  return <></>;
+  return (
+    <Wrap>
+      <Header>
+        <Title>문의사항</Title>
+
+        <WriteButton onClick={() => navi("/require/write")}>
+          문의 작성
+        </WriteButton>
+      </Header>
+
+      <Table>
+        <HeadRow>
+          <Cell flex={1}>번호</Cell>
+          <Cell flex={6}>제목</Cell>
+          <Cell flex={2}>작성일</Cell>
+        </HeadRow>
+
+        {requires.map((require) => (
+          <Row
+            key={require.requireNo}
+            onClick={() => navi(`/require/${require.requireNo}`)}
+          >
+            <Cell flex={1}>{require.requireNo}</Cell>
+
+            <Cell flex={6}>{require.requireTitle}</Cell>
+
+            <Cell flex={2}>{require.createDate}</Cell>
+          </Row>
+        ))}
+      </Table>
+
+      <Spacer />
+
+      <Pagination>
+        {page > 0 && (
+          <NextButton onClick={() => setPage(page - 1)}>이전</NextButton>
+        )}
+
+        {Array.from(
+          { length: groupEnd - groupStart },
+          (_, i) => groupStart + i,
+        ).map((pageNumber) => (
+          <PageButton
+            key={pageNumber}
+            active={page === pageNumber}
+            onClick={() => setPage(pageNumber)}
+          >
+            {pageNumber + 1}
+          </PageButton>
+        ))}
+
+        {page < totalPages - 1 && (
+          <NextButton onClick={() => setPage(page + 1)}>다음</NextButton>
+        )}
+      </Pagination>
+    </Wrap>
+  );
 };
 
 export default Require;
